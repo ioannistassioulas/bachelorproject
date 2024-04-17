7c416b761a10039047099d8fae1f96c8bb671681
I. Angelo Tassioulas <s4290755@fwn-nb4-129-125-35-233.chem.rug.nl>
2024-04-17 17:02:35 +0200

    some work on synapses

--- main.py
+++ main.py
@@ -1,24 +1,63 @@
 #
-
-
 from brian2 import *
 
 start_scope()
 
-N = 100
-tau = 10*ms
-eqs ='''
-dv/dt = (2-v)/tau : 1
-'''
+N = 10
+# eqs = '''
+# dv/dt = (current-v)/tau : 1
+# current : 1
+# tau : second
+# '''
 
-G = NeuronGroup(N, eqs, threshold='v > 1', reset = 'v = 0', refractory = 5*ms, method = 'exact')
-G.v = 'rand()'
+G = NeuronGroup(N, 'v:1')
+# G.current = [2, 0, 0]
+# G.tau = [10, 100, 100] * ms
 
-S = SpikeMonitor(G)
+synapse = Synapses(G, G)
+synapse.connect(condition='abs(i-j)<4 and i!=j')
 
-run(20*ms)
+synapse1 = Synapses(G, G)
+synapse1.connect(j='i')
+M = StateMonitor(G, 'v', record=True)
 
-plot(S.t/ms, S.i, '.k')
-xlabel('Time(ms)')
-ylabel('Neuron index')
+run(100*ms)
+
+
+def visualize_connections(S):
+    number_source = len(S.source)
+    number_target = len(S.target)
+
+    figure(figsize = (10,4))
+    subplot(121)
+
+    plot(zeros(number_source), arange(number_source), 'ok', ms = 10) #put points of sources on the map
+    plot(ones(number_target), arange(number_target), 'ok', ms = 10) #put points of targets on the map
+
+    for i, j in zip(S.i, S.j):
+        plot([0, 1], [i,j], '-k') #for each connection created by the synapse, plot a line from the source(0, source) to target (1, target)
+
+    xticks([0, 1], ['Source', 'Target']) #label of sources and targets
+    ylabel('Neuron index') #
+    xlim(-0.1, 1.1)
+    ylim(-1, max(number_source, number_target))
+
+    subplot(222)
+    plot(S.i, S.j, 'ok')
+    xlim(-1, number_source)
+    ylim(-1, number_target)
+    xlabel('Source neurons')
+    ylabel('Target neurons')
+
+visualize_connections(synapse)
+visualize_connections(synapse1)
 show()
+
+# plot(M.t/ms, M.v[0], label="Neuron 0")
+# plot(M.t/ms, M.v[1], label="Neuron 1")
+# plot(M.t/ms, M.v[2], label="Neuron 2")
+# xlabel("Time [ms]")
+# ylabel("v")
+# legend()
+
+# show()
