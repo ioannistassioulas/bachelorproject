12191c1426af4db6b97fdf4ad8829c265e7e5b59
I. Angelo Tassioulas <s4290755@fwn-nb4-129-125-35-233.chem.rug.nl>
2024-04-17 11:48:13 +0200

    practice

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-import Brian2
-volt = 20*volt
+#
+
+
+from brian2 import *
+
+start_scope()
+
+N = 100
+tau = 10*ms
+eqs ='''
+dv/dt = (2-v)/tau : 1
+'''
+
+G = NeuronGroup(N, eqs, threshold='v > 1', reset = 'v = 0', refractory = 5*ms, method = 'exact')
+G.v = 'rand()'
+
+S = SpikeMonitor(G)
+
+run(20*ms)
+
+plot(S.t/ms, S.i, '.k')
+xlabel('Time(ms)')
+ylabel('Neuron index')
+show()
