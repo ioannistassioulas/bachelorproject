0111a4dc646d7f4eb4b50b5d6135eea2122b00cf
I. Angelo Tassioulas <s4290755@fwn-nb4-129-125-35-233.chem.rug.nl>
2024-05-13 16:26:42 +0200

    hahaha

--- snnTorch_tutorial.py
+++ snnTorch_tutorial.py
@@ -1,30 +1,50 @@
+
 import snntorch as snn
+from snntorch import spikeplot as splt
+from snntorch import spikegen
+
 import torch
-from torchvision import datasets, transforms
-from snntorch import utils
+import torch.nn as nn
 
-batch_size = 128
-data_path = 'tmp/data/mnist'
-num_classes = 10
-
-transform = transforms.Compose([
-    transforms.Resize((28, 28)),
-    transforms.Grayscale(),
-    transforms.ToTensor(),
-    transforms.Normalize((0,), (1,))])
-
-mnist_train = datasets.MNIST(data_path, train=True, download=True, transform=transform)
-
-subset = 10
-mnist_train = utils.data_subset(mnist_train, subset)
-
-# Torch variables
-dtype = torch.float
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 # practice encoding spikes from visual dataset
 
-def forward_euler_lif(pot, resist, cap, current, time_step):
+
+def forward_euler_lif(v, resist=5e7, cap=1e-10, current=0, time_step=1e-3):
     time_constant = resist * cap
-    pot = pot + time_step * (current * resist - pot) / time_constant
-    return pot
+    v = v + time_step * (current * resist - v) / time_constant
+    return v
+
+
+volt = 0.9
+volt_trace = []
+
+for i in range(100):
+    volt_trace.append(volt)
+    volt = forward_euler_lif(volt)
+
+plt.plot(np.arange(100), volt_trace)
+plt.xlabel("Membrane Potential")
+plt.ylabel("Timestep")
+plt.title("Leaky Neuron Model")
+plt.show()
+
+lif1 = snn.Lapicque(R=5, C=1e-3, time_step=1e-3)
+
+mem_volt = torch.ones(1)*0.9
+current = torch.zeros(100, 1)
+spikes = torch.zeros(1)
+
+mem_rec = [mem_volt]
+for step in range(100):
+    spikes, mem_volt = lif1(current[step], mem_volt)
+    mem_rec.append(mem_volt)
+
+mem_rec = torch.stack(mem_rec)
+plt.plot(np.arange(101), mem_rec)
+plt.xlabel("Membrane Potential")
+plt.ylabel("Timestep")
+plt.title("Leaky Neuron Model")
+plt.show()
