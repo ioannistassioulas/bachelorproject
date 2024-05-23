fix broadcasting issues

audio_processing.py
@@ -202,7 +202,7 @@ def set_recording(x_values, y_values, start, stop):
     :param y_values: 2xN array of y_values of spike information, first and second waves
     :param start: value of time to start recording
     :param stop: value of time to end recording
-    :return x_values, y_values: original x and y, now properly snipped for the recording
+    :return x, y: original x and y, now properly snipped for the recording
     """
 
     # define start and stop array masks, based on time values greater than start time and less than end time
@@ -211,12 +211,15 @@ def set_recording(x_values, y_values, start, stop):
     stop_index = x_values[0] < stop
 
     # apply boolean slices onto the x values and y values
-    x_values[0] = x_values[0][np.logical_and(start_index, stop_index)]
-    y_values[0] = y_values[0][np.logical_and(start_index, stop_index)]
-    x_values[1] = x_values[1][np.logical_and(start_index, stop_index)]
-    y_values[1] = y_values[1][np.logical_and(start_index, stop_index)]
+    xf = x_values[0][np.logical_and(start_index, stop_index)]
+    yf = y_values[0][np.logical_and(start_index, stop_index)]
+    xt = x_values[1][np.logical_and(start_index, stop_index)]
+    yt = y_values[1][np.logical_and(start_index, stop_index)]
 
-    return x_values, y_values
+    x = [xf, xt]
+    y = [yf, yt]
+
+    return x, y
 
 
 def angle_itd(distance, time, speed=343):

neural_network_model.py
@@ -14,6 +14,7 @@ def forward_euler_lif(volt, current, threshold, resist=5e7, cap=1e-10, time_step
     :param cap: capacitance of the membrane
     :param threshold: voltage threshold for when spike will fire
     :param time_step: timestep to evaluate potential
+
     :return v: voltage at following timestep
     :return spike: 1 if spike is sent, 0 otherwise
     """
@@ -31,17 +32,17 @@ def forward_euler_lif(volt, current, threshold, resist=5e7, cap=1e-10, time_step
     return volt, spike
 
 
-def tde_neuron(time, facilitatory, trigger, theta, refractory=10, sr=1):
+def tde_neuron(start, time, facilitatory, trigger, theta, sr, refractory=500):
     """
     TDE model for receiving 2 spike trains and then returning corresponding
     EPSC.
-
-    :param time: how long to read for
+    :param start: timestamp of beginning of sound event
+    :param time: timestep of
     :param facilitatory: spike train of first input that is received
     :param trigger: spike train of second input that is received
     :param theta: the cutoff threshold for the LiF neuron
     :param refractory: how many timesteps to prevent before next fire
-    :param sr: sampling rate, set to 1 for simplicity atm
+    :param sr: sampling rate of audiofile
 
     :return espc: excitatory post synpatic current
     :return voltage, the recording of the voltage change of the neuron
@@ -49,17 +50,16 @@ def tde_neuron(time, facilitatory, trigger, theta, refractory=10, sr=1):
     """
 
     # define amount of timesteps to be looped over
-    timestep = time * sr
+    timestep = int(time * sr)
 
-    # convert timestamp information into spike train
     # create array of timesteps to send spikes and create spike train for facilitatory input
-    facilitatory = facilitatory * sr
+    facilitatory = (facilitatory - start) * sr  # subtract by start time such that length is consistent with timestep length
     current_f = torch.zeros(timestep)
     for j in facilitatory:
-        current_f[int(j)-1] = torch.ones(1)  # at each index of the time step you input in facilitatory array
+        current_f[int(j)] = torch.ones(1)  # at each index of the time step you input in facilitatory array
 
     # repeating for trigger input
-    trigger = trigger * sr
+    trigger = (trigger - start) * sr
     current_t = torch.zeros(timestep)
     for j in trigger:
         current_t[int(j)] = torch.ones(1)
@@ -105,10 +105,10 @@ def tde_neuron(time, facilitatory, trigger, theta, refractory=10, sr=1):
     return epsc, voltage_record, spike_record
 
 
-def tde_model(time, facilitatory, trigger, theta_input, theta_evaluate, sr=1):
+def tde_model(start, time, facilitatory, trigger, theta_input, theta_evaluate, sr):
     """
     Generate spikes via potential
-
+    :param start: timestamp of beginning of sound event
     :param time: how long the recording for spike input takes place
     :param facilitatory:  the spike chain for the facilitatory input
     :param trigger: The spike chain for the trigger input
@@ -120,10 +120,10 @@ def tde_model(time, facilitatory, trigger, theta_input, theta_evaluate, sr=1):
     """
 
     # define timesteps where model will be defined over
-    timesteps = sr * time
+    timesteps = int(sr * time)
 
     # Generate voltage for LiF model
-    epsc, membrane_volt, spike = tde_neuron(time, facilitatory, trigger, theta_input)
+    epsc, membrane_volt, spike = tde_neuron(start, time, facilitatory, trigger, theta_input, sr)
 
     # feed membrane recording as input for new neuron
     final_spikes = []
@@ -133,33 +133,33 @@ def tde_model(time, facilitatory, trigger, theta_input, theta_evaluate, sr=1):
 
     return epsc, spike, membrane_volt, final_spikes
 
-
-# current_fac = torch.cat((torch.zeros(10, 1), torch.ones(1, 1), torch.zeros(189, 1), torch.ones(1, 1), torch.zeros(199, 1)), 0)
-# current_trig = torch.cat((torch.zeros(15, 1), torch.ones(1, 1), torch.zeros(189, 1), torch.ones(1, 1), torch.zeros(194, 1)), 0)
-time1 = 100
-current_fac = np.array([11, 25, 71])
-current_trig = np.array([13, 27, 73])
-# model the TDE reaction to above inputs
-# post_synaptic, v, i = tde_neuron(time1, current_fac, current_trig, 1e20)
-
-# Final LiF neuron modeling the voltage
-post_synaptic, i, v, spikes = tde_model(time1, current_fac, current_trig, 1e20, 2e6)
-
-# it works!
-# plot out the dataset from what was created
-fig, ax = plt.subplots(2, 1)
-ax[0].plot(np.arange(len(post_synaptic)), post_synaptic, color="blue", label="recording")
-ax[0].scatter(current_fac, [1e7] * len(current_fac), color="red", marker="x", label="current")
-ax[0].scatter(current_trig, [1e7] * len(current_trig), color="blue", marker="x", label="trigger")
-ax[0].plot(np.arange(len(i)), i, label="tde triggering")
-ax[0].plot(np.arange(len(v)), v, color="red", label="voltage")
-# ax[0].axhline(thresh, color="black", label=f"threshold={thresh}")
-ax[0].legend()
-
-ax[1].plot(np.arange(len(post_synaptic)), np.array(post_synaptic), label="voltage")
-ax[1].axhline(2e6, color="black", label="threshold")
-ax[1].scatter(np.arange(len(spikes)), np.array(spikes) * 2e6, label="spikes", marker="|", color="grey")
-
-fig.suptitle("Spiking behavior of TDE model")
-plt.legend()
-plt.show()
+#
+# # current_fac = torch.cat((torch.zeros(10, 1), torch.ones(1, 1), torch.zeros(189, 1), torch.ones(1, 1), torch.zeros(199, 1)), 0)
+# # current_trig = torch.cat((torch.zeros(15, 1), torch.ones(1, 1), torch.zeros(189, 1), torch.ones(1, 1), torch.zeros(194, 1)), 0)
+# time1 = 100
+# current_fac = np.array([11, 25, 71])
+# current_trig = np.array([13, 27, 73])
+# # model the TDE reaction to above inputs
+# # post_synaptic, v, i = tde_neuron(time1, current_fac, current_trig, 1e20)
+#
+# # Final LiF neuron modeling the voltage
+# post_synaptic, i, v, spikes = tde_model(time1, current_fac, current_trig, 1e20, 2e6)
+#
+# # it works!
+# # plot out the dataset from what was created
+# fig, ax = plt.subplots(2, 1)
+# ax[0].plot(np.arange(len(post_synaptic)), post_synaptic, color="blue", label="recording")
+# ax[0].scatter(current_fac, [1e7] * len(current_fac), color="red", marker="x", label="current")
+# ax[0].scatter(current_trig, [1e7] * len(current_trig), color="blue", marker="x", label="trigger")
+# ax[0].plot(np.arange(len(i)), i, label="tde triggering")
+# ax[0].plot(np.arange(len(v)), v, color="red", label="voltage")
+# # ax[0].axhline(thresh, color="black", label=f"threshold={thresh}")
+# ax[0].legend()
+#
+# ax[1].plot(np.arange(len(post_synaptic)), np.array(post_synaptic), label="voltage")
+# ax[1].axhline(2e6, color="black", label="threshold")
+# ax[1].scatter(np.arange(len(spikes)), np.array(spikes) * 2e6, label="spikes", marker="|", color="grey")
+#
+# fig.suptitle("Spiking behavior of TDE model")
+# plt.legend()
+# plt.show()

tau_dataset_spike_encoding.py
@@ -1,8 +1,13 @@
 import audio_processing as ap
 from audio_processing import *
 
-import snnTorch_tutorial as sn
-from snnTorch_tutorial import *
+import neural_network_model as sn
+from neural_network_model import *
+
+import time as t
+# start recording time
+start_time = t.time()
+
 # get current directory
 home = os.getcwd()
 one_up = path.abspath(path.join(home, ".."))
@@ -11,13 +16,13 @@ audio_tau_loc = one_up + "/mic_dev/"
 
 # retrieve all filenames
 metadata_tau = os.listdir(metadata_tau_loc)
+print(f"Acquiring sound files done! Time elapsed = {t.time() - start_time}s")
 
 # array of all files most important info
 # setup of array : [audio file, sound_event, metadata of event]
 keypoints = []
 stereo = []
 sr = 0
-k = 1
 
 # start parsing through csv file and saving information
 for file in metadata_tau:  # parse through each audio split
@@ -48,23 +53,24 @@ for file in metadata_tau:  # parse through each audio split
         end.append(sound[2])
         angle.append(sound[3])
         distance.append(sound[5])
-        print(f"Running . . . Iteration number {k}")
-        k += 1
 
     # add onto keypoints mega-array
     file_info = [start, end, angle, distance]
     keypoints.append(file_info)
 
+# print complete message to timestamp
+print(f"Reading data done! Time elapsed = {t.time() - start_time}s")
 
-# start looking into the keypoints mega-array
-print("Done!")
-
+# create memory arrays to hold all itd and ild values
 time_difference = []
 level_difference = []
-time = []
+times = []
+gt = []
+# index to parse through file
 index = 0
 for file in keypoints:
 
+    timestamp = t.time()
     # select sound_file to analyze
     sound_data = stereo[index]
 
@@ -74,11 +80,16 @@ for file in keypoints:
     itd_x, itd_y = ap.zero_crossing(waves, sr)
     ild_x, ild_y = ap.peak_difference(waves, sr)
 
-    # fix any issues in broadcasting
-    itd_x, itd_y = ap.fix_broadcasting(itd_x, itd_y)
-    ild_x, ild_y = ap.fix_broadcasting(ild_x, ild_y)
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(np.arange(len(waves[0])), waves[0])
+    ax[1].plot(np.arange(len(waves[1])), waves[1])
+    ax[0].scatter(itd_x[0] * sr, itd_y[0])
+    ax[1].scatter(itd_x[1] * sr, itd_y[1])
+    plt.show()
 
-    for split in file:
+    jindex = 0
+    for split in np.array(file).transpose():
+
         # import metadata for ground-truth comparison
         start = split[0]
         end = split[1]
@@ -89,22 +100,51 @@ for file in keypoints:
         xt, yt = ap.set_recording(itd_x, itd_y, start, end)
         xl, yl = ap.set_recording(ild_x, ild_y, start, end)
 
+        # fix any issues in broadcasting
+        xt, yt = ap.fix_broadcasting(xt, yt)
+        xl, yl = ap.fix_broadcasting(xl, yl)
+
         # append critical values into time difference array
-        time_difference.append([xt, yt])
-        level_difference.append([xl, yl])
-        time.append(end-start)
-    # iterate the index of the sound_file parser
+        time_difference.append(xt)
+        print(xt)
+        level_difference.append(yt)
+        times.append([start, end])
+        gt.append([ground_truth_theta, ground_truth_distance, f"sound file number {index + 1}, sound event number {jindex + 1}, timespan = {start}s to {end}s"])
+        jindex += 1
+
+    # iterate the index of the sound_file
+    print(f"Split number {index + 1} done! Time elapsed = {t.time() - timestamp}s")
     index += 1
 
-print("zero-encoding complete!")
+# timestamp to find out when zero encoding is done
+print(f"zero-encoding complete! Total time elapse = {t.time() - start_time}s")
 
 # start sending data in to run for spikes
-for i in range(len(time)):
-    #
-    timespan = time[i]
-    facilitatory_time, trigger_time = time_difference[i]
+for i in range(len(times)):
+    start = times[i][0]  # time of recording starting, prevents OOB errors
+    timespan = times[i][1] - times[i][0]  # timespan in numbers
+    facilitatory_time, trigger_time = time_difference[i]  # save itd and ild info into holder variables for readability
     facilitatory_level, trigger_level = level_difference[i]
 
-    post_synaptic, i, v, spikes = sn.tde_model(timespan, facilitatory_time, trigger_time, 1e20, 1e20, sr)
+    # start running tde_model
+    post_synaptic, i, v, spikes = sn.tde_model(start, timespan, facilitatory_time, trigger_time, 1e20, 1e20, sr)
 
-    plt.plot()
+    print(f"TDE modeling complete! Total Time elapse = {t.time() - start_time}s")
+
+    # plot out dataset
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(np.arange(len(post_synaptic)), post_synaptic, color="blue", label="recording")
+    ax[0].scatter((facilitatory_time - start) * sr, [1e7] * len((facilitatory_time - start) * sr), color="red", marker="x", label="current")
+    ax[0].scatter((trigger_time - start) * sr, [1e7] * len((trigger_time - start) * sr), color="blue", marker="x", label="trigger")
+    ax[0].plot(np.arange(len(i)), i, label="tde triggering")
+    ax[0].plot(np.arange(len(v)), v, color="red", label="voltage")
+    # ax[0].axhline(thresh, color="black", label=f"threshold={thresh}")
+    ax[0].legend()
+
+    ax[1].plot(np.arange(len(post_synaptic)), np.array(post_synaptic), label="voltage")
+    ax[1].scatter(np.arange(len(spikes)), np.array(spikes) * 2e6, label="spikes", marker="|", color="grey")
+
+    fig.suptitle(gt[i][2])
+    plt.legend()
+    plt.show()
+