31ce07a6c440d3747ede853a8a1a692533bd4514
ioannistassioulas <i.a.tassioulas@gmail.com>
2024-06-21 00:22:31 +0200

    ready for sending to habrok

--- tau_dataset_spike_encoding.py
+++ tau_dataset_spike_encoding.py
@@ -22,8 +22,7 @@ print(f"Acquiring sound files done! Time elapsed = {t.time() - start_time}s")
 # array of all files most important info
 # setup of array : [audio file, sound_event, metadata of event]
 keypoints = []
-stereo = []
-sr = 14000
+sr = 1000
 # start parsing through csv file and saving information
 for file in metadata_tau:  # parse through each audio event
     # get names of files
@@ -37,11 +36,10 @@ for file in metadata_tau:  # parse through each audio event
 
     intensitiesBefore = np.array([intensities.transpose()[0], intensities.transpose()[2]])
     intensitiesAfter = np.array([signal.resample(intensitiesBefore[0], samp), signal.resample(intensitiesBefore[1], samp)])
-
-    stereoCSV = pd.DataFrame(np.array([intensitiesBefore, intensitiesAfter]))
-
-    header = ["Before-Down-Sampling", "After-Down-Sampling"]
-    stereoCSV.to_csv(f"{root_name}-down-sampling-example.csv", columns=header)
+    a = {"IBL": intensitiesBefore[0], "IBR": intensitiesBefore[1],
+                              "IAL": intensitiesAfter[0], "IAR": intensitiesAfter[1]}
+    stereoCSV = pd.DataFrame.from_dict(a, orient='index').transpose()
+    stereoCSV.to_csv(f"results/{root_name}-down-sampling.csv")
 
     # start reading through specific csv file
     meta_csv = pd.read_csv(meta)
@@ -72,6 +70,11 @@ print(f"Reading data done! Time elapsed = {t.time() - start_time}s")
 
 # START LOOPING THROUGH FILES AND EVENTS
 for i in range(len(metadata_tau)):  # start looking at each .wav file
+    root_name = metadata_tau[i].partition('.')[0]
+    stereo = pd.read_csv(f"results/{root_name}-down-sampling.csv")
+    sig = [stereo['IAL'].to_numpy(), stereo['IAR'].to_numpy()]
+    sig = [sig[0][~np.isnan(sig[0])], sig[1][~np.isnan(sig[1])]]
+    print(sig)
     for j in range(1):  # go through all sound events
 
         start = keypoints[i][j][0]
@@ -81,7 +84,6 @@ for i in range(len(metadata_tau)):  # start looking at each .wav file
         distance = keypoints[i][j][4]
 
         # Practice filtering out all the data
-        sig = [stereo[i][0], stereo[i][2]]
         timespan = [np.arange(len(np.array(sig[0]))), np.arange(len(np.array(sig[1])))]
 
         # look at events
@@ -105,64 +107,26 @@ for i in range(len(metadata_tau)):  # start looking at each .wav file
         main_freq_r = np.abs(freq_fft[wave_fft[1].argmax()])  # main frequency
 
         # header : frequency range, left ear fft, right ear fft, left peak x, left peak y, right peak x, right peak y
-        freq_info = pd.DataFrame([freq_fft, wave_fft[0], wave_fft[1], main_freq_l, l_max, main_freq_r, r_max])
-        print(freq_info)
-        freq_info.to_csv(f"frequency_info_Split-{i+1}_Event-{j+1}.csv", header=["frequency range", "left ear fft, right ear fft, left peak x, left peak y, right peak x, right peak y"])
+        b = {"Freq-Range": freq_fft, "Left-FFT": wave_fft[0], "Right-FFT": wave_fft[1]}
 
+        freq_info = pd.DataFrame.from_dict(b, orient="index").transpose()
+        freq_info.to_csv(f"results/Fourier-Results-Split-{i+1}-Event-{j+1}.csv")
         avg_freq = int(np.mean([main_freq_l, main_freq_r]))
         freq_band = [avg_freq - 5, avg_freq + 5]
 
         waves = audio_processing.filter_waves(waves, freq_band, "bandpass")
         print(f"Filtering complete! Time elapsed = {t.time() - start_time}s")
 
-        # sanity check to make sure waves still look good
-        wave_fft = fft.fft(waves)  # peaks of fft transform
-        freq_fft = fft.fftfreq(len(timespan[0]), 1 / sr)  # frequencies to check over
-
-        l_max = np.max(wave_fft[0])
-        r_max = np.max(wave_fft[1])
-        main_freq_l = np.abs(freq_fft[wave_fft[0].argmax()])  # main frequency
-        main_freq_r = np.abs(freq_fft[wave_fft[1].argmax()])  # main frequency
-
-        # fig, ax = plt.subplots(1, 2, sharex=True, sharey=True)
-        # ax[0].plot(freq_fft, wave_fft[0], color="black")
-        # ax[0].scatter(main_freq_l, l_max, color="red", label=f"Primary Frequency = {main_freq_l}")
-        # ax[1].plot(freq_fft, wave_fft[1], color="black")
-        # ax[1].scatter(main_freq_r, r_max, color="red", label=f"Primary Frequency = {main_freq_r}")
-        # fig.text(0.5, 0.04, 'Frequency', ha='center')
-        # fig.text(0.04, 0.5, 'Fourier transform of wave', va='center', rotation='vertical')
-        # fig.suptitle("Frequency spectrum of wave obtained via FFT")
-        # ax[0].legend()
-        # ax[1].legend()
-        # plt.show()
-
         # calculate all zero crossings
         zero_x, zero_y = audio_processing.zero_crossing(waves, sr)
         zero_x[0] = zero_x[0] + start  # fix problems with zeros and linmes crossing on the bar
         zero_x[1] = zero_x[1] + start
         zero_x, zero_y = audio_processing.fix_broadcasting(zero_x, zero_y)
+
+        c = {"ZL": zero_x[0], "ZR": zero_x[1], "WL": waves[0], "WR": waves[1]}
+        zero_data = pd.DataFrame.from_dict(c, orient="index").transpose()
+        zero_data.to_csv(f"results/zero_crossings_data_Split{i+1}_Split{j+1}.csv")
         print(f"Zero crossing complete! Time elapsed = {t.time() - start_time}s")
-
-        zero_data = pd.DataFrame([zero_x[0], waves[0], zero_x[1], waves[1]])
-        zero_data.to_csv(f"zero_crossings_data.csv", header=["left-zero, left-audio, right-zero, right-audio"])
-
-        # plot to show effectiveness of filtering
-        # fig, ax = plt.subplots(2, 1)
-        #
-        # ax[0].plot(np.array(time_unfiltered) / sr, unfiltered, color="red")
-        # ax[0].set_title("Unfiltered sound event")
-        #
-        # ax[1].plot(timespan[0] / sr, waves[0], color="blue", label="left")
-        # ax[1].scatter(zero_x[0], [0] * len(zero_x[0]), color="black", label="zero crossings")
-        # ax[1].plot(timespan[1] / sr, waves[1], color="green", label="right")
-        # ax[1].scatter(zero_x[1], [0] * len(zero_x[1]), color="grey", label="zero crossings")
-        # ax[1].set_title("Filtered sound event")
-        #
-        # fig.suptitle(f"Effect of filtering soundwaves, bandpass of {freq_band} Hz. Split 1, Event 1")
-        # plt.legend()
-        # plt.show()
-
-        print(f"First graph complete! Time elapsed = {t.time() - start_time}s")
 
         # delete all time intervals that are higher than expected (due to fault)
         time_differences = np.abs(zero_x[1] - zero_x[0])  # experimental phase crossing
@@ -175,14 +139,14 @@ for i in range(len(metadata_tau)):  # start looking at each .wav file
         # transfer zeros into spike train
         zero_x[0] = (zero_x[0] - start) * sr  # subtract by start time such that length is consistent with timestep length
         current_f = torch.zeros(len(timespan[0]))
-        for j in zero_x[0]:
-            current_f[int(j)] = torch.ones(1)  # at each index of the time step you input in facilitatory array
+        for j1 in zero_x[0]:
+            current_f[int(j1)] = torch.ones(1)  # at each index of the time step you input in facilitatory array
 
         # repeating for trigger input
         zero_x[1] = (zero_x[1] - start) * sr
         current_t = torch.zeros(len(timespan[1]))
-        for j in zero_x[1]:
-            current_t[int(j)] = torch.ones(1)
+        for j2 in zero_x[1]:
+            current_t[int(j2)] = torch.ones(1)
 
         # pass spike train into tde simulator
 
@@ -191,21 +155,8 @@ for i in range(len(metadata_tau)):  # start looking at each .wav file
         timer = int((end - start) * sr)
 
         mem, spk, fac, trg = tde(tau_tde, tau_tde, tau_mem, torch.tensor(1/sr), torch.tensor(timer - 1), current_f, current_t)
-
-        # plot spiking behaviour of first part
-        fig, ax = plt.subplots(2, 1)
-        ax[0].plot(np.arange(timer - 1), fac[0], label="facilitatory")
-        ax[0].plot(np.arange(timer - 1), trg[0], label="trigger")
-        ax[0].legend()
-        ax[0].set_title("TDE recording portion")
-
-        ax[1].plot(np.arange(timer - 1), mem[0], label="membrane")
-        ax[1].plot(np.arange(timer - 1), spk[0], label="spike")
-        ax[1].legend()
-        ax[1].set_title("Membrane and Spiking behaviour")
-
-        fig.suptitle("Spiking behaviour for Split 1, Event 1")
-        plt.show()
+        tde_results = pd.DataFrame([mem[0], spk[0], fac[0], trg[0]])
+        tde_results.to_csv(f"results/LIF-Response-Split{i+1}-Event-{j+1}.csv")
 
         # start counting all spikes
         total_spike = torch.tensor(signal.convolve(spk[0], mem[0]))
@@ -214,9 +165,12 @@ for i in range(len(metadata_tau)):  # start looking at each .wav file
         # add spike count to final counter alongside the required angle
         angle = 45 - angle
         results = pd.DataFrame([spike_count, angle, elevation, distance])
-        results.DataFrame(f"split_{i+1}-event_{j+1}.csv", header=["Spiking data, Angle, Elevation, Distance"])
+        results.to_csv(f"results/final_split_{i+1}-event_{j+1}.csv")
         
         # throw away to try and save a bit of memory
         gc.collect()
 
-
+# l_max = np.max(wave_fft[0])
+# r_max = np.max(wave_fft[1])
+# main_freq_l = np.abs(freq_fft[wave_fft[0].argmax()])  # main frequency
+# main_freq_r = np.abs(freq_fft[wave_fft[1].argmax()])  # main frequency
